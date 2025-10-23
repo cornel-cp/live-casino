4336d47470c14bf8dcc49237b4bb3c7e75ab0ab5
Cornel <172536691+cornel-cp@users.noreply.github.com>
2025-10-23 19:19:49 +0900

    footer_complete

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,9 +1,77 @@
+"use client"
+
+import Link from "next/link";
+import { useState } from "react"
+
+
 const Footer = () => {
 
+    const [data, setData] = useState({
+        name: "",
+        email: ""
+    });
     return (
-        <footer className="bg-red-400 pt-12 pb-20 px-2 sm:px-4 md:px-8">
-            this is footer
-        </footer>
+        <footer className="">
+            <div className="bg-[url(/assets/images/footer-bg1.png)]  bg-black bg-no-repeat bg-cover w-full h-[140px]">
+                <div className="bg-[url(/assets/images/footer-gradient.png)] w-full bg-cover h-[140px] bg-no-repeat opacity-75">
+                    <div className="flex">
+                        <div className="flex-6 px-[55px] pt-8 pb-0">
+                            <p className="text-white font-normal text-[40px] pt-[2.5px] m-0">CLUB 8 REWARDS</p>
+                            <p className="text-white text-[18px] pt-0.5 m-0">GET FRESH NEWS EVERY MONTH!</p>
+                        </div>
+                        <div className="flex-6 flex-col">
+                            <form className="pt-[41px] pb-[41px] flex z-50 items-center justify-around" action={"#"}>
+                                <div>
+                                    <input className="bg-white/20 block w-[207px] h-6 text-white mb-2.5 px-2 py-1 border-ra" value={data.name} onChange={(e) => {
+                                        setData({ ...data, name: e.target.value })
+                                    }} placeholder="Your Name" />
+
+                                    <input className="bg-white/20 w-[207px] h-6 text-white px-2 py-1" value={data.email} onChange={(e) => {
+                                        setData({ ...data, email: e.target.value })
+                                    }} placeholder="Your Email" />
+                                </div>
+                                <button className="w-[103px] h-[34px] bg-red-800 -skew-x-16" type="submit"><p className="skew-x-16 text-white"> Subscribe</p></button>
+                            </form>
+                        </div>
+                    </div></div>
+            </div>
+            <div className="bg-[url(/assets/images/footer-bg2.png)] bg-black bg-no-repeat bg-cover flex flex-col justify-top w-full h-[535px]">
+                <div className="bg-[url(/assets/images/footer_bg2_gradient.png)] w-full bg-cover h-[267px] bg-no-repeat"></div>
+                <div className="px-16">
+                    <div className="flex items-center">
+                        <img src={"/assets/images/footer_mobile.png"} alt="mobile" width={"24px"} height={"24px"} />
+                        <p className="text-white pl-2 text-[10px] font-bold">408 244 3333</p>
+                    </div>
+                    <div className="flex items-center justify-between mt-5">
+                        <div className="flex">
+                            <img src={"/assets/images/footer_location.png"} alt="location" width={"24px"} height={"24px"} />
+                            <div className="text-white pl-2 text-[12px] ">
+                                <p className="font-bold">1887 Matrix Boulevard San Jose, Ca 95110</p>
+                                <p className="font-normal">We are just off Highway 101 by the San Jose International Airport.</p>
+                            </div>
+                        </div>
+                        <button className="border-2 border-white -skew-x-16 text-white p-2"><p className="sk">Private Policy</p></button>
+                    </div>
+                    <div className="flex items-center justify-between mt-5">
+                        <div className="flex text-white text-[9px]">
+                            <img src={"/assets/images/footer-logo.png"} alt="location" width={"120px"} height={"32px"} />
+                            <div className="pl-2.5">
+                                <p >© 2022 CASINO M8TRIX. All Rights Reserved</p>
+                                <p>21+ Please gamble responsibly | 1-800-GAMBLER | www.problemgambling.ca.gov</p>
+                                <p>This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
+                            </div>
+                        </div>
+                        <div className="flex gap-7">
+                            <img src={"/assets/images/facebook.png"} alt="facebook" width={"12px"} height={"22px"} />
+                            <img src={"/assets/images/instagram.png"} alt="instagram" width={"22px"} height={"22px"} />
+                            <img src={"/assets/images/Youtube.png"} alt="Youtube" width={"26px"} height={"20px"} />
+                            <img src={"/assets/images/Linkedin.png"} alt="Linkedin" width={"23px"} height={"22px"} />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer >
     )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         />
       </Head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[895px] m-auto`}
       >
         {children}
       </body>
