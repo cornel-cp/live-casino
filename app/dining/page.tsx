b610690efa110fcfc972fd5bcb9b3ee91d1f994d
Cornel <172536691+cornel-cp@users.noreply.github.com>
2025-10-23 22:12:53 +0900

    Dining Room Complete

--- app/dining/page.tsx
+++ app/dining/page.tsx
@@ -8,7 +8,12 @@ const HomePage = () => {
         <Layout>
             {/* room Section */}
             <div className="bg-[url(/assets/images/bg_dining_room.jpg)] bg-no-repeat bg-cover h-[469px] flex flex-col justify-end w-full" >
-                <div className="bg-[url(/assets/images/bggradient_home.png)] bg-no-repeat bg-cover w-full h-[469px]"></div>
+                <div className="flex items-end justify-center bg-[url(/assets/images/bggradient_home.png)] bg-no-repeat bg-cover w-full h-[469px]">
+                    <div className="">
+                        <p className="text-[100px] font-normal text-white">DINING</p>
+                        <p className="text-[28px] font-normal pt-0 text-red-600 text-center pb-[69px]">AT CASINO M8TRIX</p>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
