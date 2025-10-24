033bbd017330d8b2ed31eb6d85de2f59d8d6bac3
Cornel <172536691+cornel-cp@users.noreply.github.com>
2025-10-24 19:28:57 +0900

    Dining SportsBar page init

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -1,43 +1,51 @@
-"use client"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { useEffect } from "react"
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const Navbar = () => {
+  const path = usePathname();
 
-    const path = usePathname()
+  const menulist = [
+    {
+      label: "Home",
+      path: "/home",
+    },
+    {
+      label: "Gaming",
+      path: "/gaming",
+    },
+    {
+      label: "Dining",
+      path: "/dining",
+    },
+    {
+      label: "Employment",
+      path: "/employment",
+    },
+  ];
 
-    const menulist = [
+  useEffect(() => {
+    console.log(path);
+  }, []);
 
-        {
-            label: "Home",
-            path: "/home",
-        },
-        {
-            label: "Gaming",
-            path: "/gaming",
-        },
-        {
-            label: "Dining",
-            path: "/dining",
-        },
-        {
-            label: "Employment",
-            path: "/employment",
-        },
-    ]
+  return (
+    <ul className="flex gap-2 py-4 px-10 bg-white/20">
+      {menulist.map((menu, idx) => (
+        <Link
+          href={menu.path}
+          className={`uppercase  text-[14px] cursor-pointer ${
+            "/" + path.split("/")[1] === menu.path
+              ? "text-white/50"
+              : "text-white"
+          }`}
+          key={idx}
+        >
+          {menu.label}
+        </Link>
+      ))}
+    </ul>
+  );
+};
 
-    // useEffect(() => {
-    //     console.log(path)
-    // }, [])
-
-    return (
-        <ul className="flex gap-2 py-4 px-10 bg-white/20 -skew-x-12 mr-11">
-            {menulist.map((menu, idx) => (
-                <Link href={menu.path} className={`uppercase  text-[14px] cursor-pointer skew-x-12  ${path === menu.path ? "text-white/20" : "text-white"}`} key={idx}>{menu.label}</Link>
-            ))}
-        </ul>
-    )
-}
-
-export default Navbar
+export default Navbar;
---
+++ app/dining/sportsBar/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Layout from "@/app/components/layout/layout";
+
+const SportsBar = () => {
+  return (
+    <Layout>
+      <div className="bg-[url(/assets/images/bg_dining_room.jpg)] bg-no-repeat bg-cover h-[469px] flex flex-col justify-end w-full">
+        <div className="flex items-end justify-center bg-[url(/assets/images/bggradient_home.png)] bg-no-repeat bg-cover w-full h-[469px]"></div>
+      </div>
+    </Layout>
+  );
+};
+
+export default SportsBar;
