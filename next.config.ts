352e2d2b23ae3abf7565116f9d09c341144a0fa7
Cornel <172536691+cornel-cp@users.noreply.github.com>
2025-10-26 00:46:22 +0900

    repush

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  reactStrictMode: true, // Enables React Strict Mode
+  env: {
+    CUSTOM_API_URL: process.env.CUSTOM_API_URL, // Custom environment variables
+  },
 };
 
-export default nextConfig;
+export default config;
+
