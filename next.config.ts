11e207ad0bbd65a8ad090894c1723aaacf066612
Cornel <172536691+cornel-cp@users.noreply.github.com>
2025-10-26 01:04:24 +0900

    Config Conflict

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,10 @@
 import { NextConfig } from 'next';
 
-<<<<<<< HEAD
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  env: {
-    CUSTOM_API_URL: process.env.CUSTOM_API_URL
-  }
-=======
 const config: NextConfig = {
   reactStrictMode: true, // Enables React Strict Mode
   env: {
     CUSTOM_API_URL: process.env.CUSTOM_API_URL, // Custom environment variables
   },
->>>>>>> 1ab5b9e33957497c30b251b5aec6c53f8b05942e
 };
 
 export default config;
